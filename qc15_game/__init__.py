4a103d74340b260a0d8051f9632010a580440655
George Louthan <duplico@dupli.co>
2018-07-18 23:14:42 -0500

    BIG REWRITE for the new-style front-end. Time to start on the backend...

---
+++ qc15_game/__init__.py
@@ -0,0 +1,11 @@
+REQUIRED_HEADINGS = ['Input_type', 'Input_detail', 'Choice_share', 
+                     'Result_duration', 'Result_type', 'Result_detail']
+
+IGNORE_STATES = ['EXAMPLE_NOTPARSED', 'SHEETNAMES']
+VALID_INPUT_TYPES = ['ENTER', 'USER_IN', 'BUTTON', 'NET', 'TIMER', 'TIMER_R',
+                     'CONTD']
+IGNORE_INPUT_TYPES = ['', 'COMMENT', 'ACTIONS']
+VALID_RESULT_TYPES = ['TEXT', 'SET_ANIM_TEMP', 'SET_ANIM_BG', 
+                      'STATE_TRANSITION', 'OTHER', 'CLOSE', 'POP']
+RESERVED_STATES = ['POP', 'POST']
+SPECIAL_STATES = ['POP', 'GLOBAL']
