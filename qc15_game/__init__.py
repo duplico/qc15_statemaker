ac0316b77344bdfd6e4fda8f813f20512598aaf0
George Louthan <duplico@dupli.co>
2018-07-19 21:05:08 -0500

    Add NOP, per TODO.

--- qc15_game/__init__.py
+++ qc15_game/__init__.py
@@ -3,7 +3,7 @@ REQUIRED_HEADINGS = ['Input_type', 'Input_detail', 'Choice_share',
 
 IGNORE_STATES = ['EXAMPLE_NOTPARSED', 'SHEETNAMES']
 VALID_INPUT_TYPES = ['ENTER', 'USER_IN', 'BUTTON', 'NET', 'TIMER', 'TIMER_R',
-                     'CONTD']
+                     'CONTD', 'NOP']
 IGNORE_INPUT_TYPES = ['', 'COMMENT', 'ACTIONS']
 VALID_RESULT_TYPES = ['TEXT', 'SET_ANIM_TEMP', 'SET_ANIM_BG', 
                       'STATE_TRANSITION', 'OTHER', 'CLOSE', 'POP']
