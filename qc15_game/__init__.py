0f4962597f577a1c6285c10350958abb721098cc
George Louthan <georgerlouth@nthefourth.com>
2018-07-23 02:22:29 -0500

    Allow the ability to handle NET inputs, possibly others, per #120.

--- qc15_game/__init__.py
+++ qc15_game/__init__.py
@@ -9,8 +9,26 @@ VALID_RESULT_TYPES = ['TEXT', 'SET_ANIM_TEMP', 'SET_ANIM_BG',
                       'STATE_TRANSITION', 'OTHER', 'CLOSE', 'POP', 'PUSH',
                       'PREVIOUS', 'NOP']
 
+RESULT_TYPE_OUTPUT = {
+    'SET_ANIM_TEMP' : 0,
+    'SET_ANIM_BG' : 1,
+    'STATE_TRANSITION' : 2,
+    'PUSH' : 3,
+    'POP' : 4,
+    'PREVIOUS' : 5,
+    'CLOSE' : 6,
+    'NOP' : 7,
+    'TEXT' : 16,
+    'TEXT_BADGNAME' : 17,
+    'TEXT_USERNAME' : 18,
+    'TEXT_CNT' : 19,
+    'OTHER' : 100
+}
+                      
 ALLOWED_VARIABLES = {
     'badgname' : '%s',
     'username' : '%s',
     'cnt' : '%d',
 }
+
+NULL = 0xFFFF
